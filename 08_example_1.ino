b979ab5e268c419580bc5c452ff1f1fdde764dcc
nnyouung <104901660+nnyouung@users.noreply.github.com>
2022-10-10 21:40:34 +0900

    Add files via upload

---
+++ 08_example_1.ino
@@ -0,0 +1,78 @@
+// Arduino pin assignment
+#define PIN_LED 9
+#define PIN_TRIG 12   // sonar sensor TRIGGER
+#define PIN_ECHO 13   // sonar sensor ECHO
+
+// configurable parameters
+#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
+#define INTERVAL 25      // sampling interval (unit: msec)
+#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
+#define _DIST_MIN 100.0   // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 300.0   // maximum distance to be measured (unit: mm)
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
+#define SCALE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
+
+unsigned long last_sampling_time;   // unit: msec
+
+void setup() {
+  // initialize GPIO pins
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);  // sonar TRIGGER
+  pinMode(PIN_ECHO, INPUT);   // sonar ECHO
+  digitalWrite(PIN_TRIG, LOW);  // turn-off Sonar 
+  
+  // initialize serial port
+  Serial.begin(57600);
+}
+
+void loop() {
+  float distance;
+  float before_distance;
+  if (millis() < (last_sampling_time + INTERVAL))
+    return;
+
+  distance = USS_measure(PIN_TRIG, PIN_ECHO); // read distance
+  if (distance <= _DIST_MIN){
+    distance = _DIST_MIN - 10.0;  
+    analogWrite(PIN_LED, 255);
+  }
+  else if(distance >= _DIST_MAX){
+    distance = _DIST_MAX + 10.0;  
+    analogWrite(PIN_LED, 255);
+  }
+  else{
+    int led = 0;
+    if(distance == 200){
+      led = 0;
+    }
+     else{
+      led = int(abs(200-distance) * 2.56);
+     }
+     analogWrite(PIN_LED, led);
+    }
+
+
+  // output the distance to the serial port
+  Serial.print("Min:");        Serial.print(_DIST_MIN);
+  Serial.print(",distance:");  Serial.print(distance);
+  Serial.print(",Max:");       Serial.print(_DIST_MAX);
+  Serial.println("");
+  
+  // do something here
+  //delay(50); // Assume that it takes 50ms to do something.
+  
+  // update last sampling time
+  last_sampling_time += INTERVAL;
+  before_distance = distance;
+}
+
+// get a distance reading from USS. return value is in millimeter.
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE; // unit: mm
+}
